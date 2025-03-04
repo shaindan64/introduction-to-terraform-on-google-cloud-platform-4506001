0987c34b7018a6fd6b13431bba8b22046e3e45f1
shaindan64 <dshain3568@aol.com>
2025-03-04 12:45:51 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,35 @@
 resource "google_compute_network" "app" {
   name                    = "app-network"
-  auto_create_subnetworks = false
+  auto_create_subnetworks  = false
 }
 
 resource "google_compute_subnetwork" "app" {
   name          = "app-subnet"
   ip_cidr_range = "10.2.0.0/16"
-  region        = "us-west1-a"
-  network       = google_compute_network.custom-test.id
-  }
-
+  region        = "us-west1"  # Specify the region, not the availability zone
+  network       = google_compute_network.app.id  # Correct network reference
+}
 
 data "google_compute_image" "ubuntu" {
   most_recent = true
-  project     = "ubuntu-os-cloud" 
+  project     = "ubuntu-os-cloud"
   family      = "ubuntu-2204-lts"
 }
 
 resource "google_compute_instance" "web" {
   name         = "web"
   machine_type = "e2-micro"
-}
-  
+
   boot_disk {
     initialize_params {
       image = data.google_compute_image.ubuntu.self_link
     }
   }
+
   network_interface {
-   subnetwork = "app"
-   access_config {
+    subnetwork = google_compute_subnetwork.app.id  # Use subnetwork reference
+    access_config {
       # Leave empty for dynamic public IP
     }
-  }  
-
+  }
 }
